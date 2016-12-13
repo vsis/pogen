33fb68fee4cbe262ca595d5ddf9fd02210995e84
Roddy Gonzalez <roddy.gonzalez.89@gmail.com>
2016-12-13 17:44:13 -0300

    Fixed pos/views.py

--- pos/views.py
+++ pos/views.py
@@ -6,7 +6,7 @@ from .models import Area, Provider, PurchaseOrder
 class ListView(generic.ListView):
     template_name = 'pos/pos_list.html'
     context_object_name = 'pos_list'
-    
+ 
     def get_queryset(self):
         return PurchaseOrder.objects.all()
 
@@ -15,12 +15,9 @@ class DetailView(generic.DetailView):
     template_name = 'pos/detail.html'
 
 def new_po(request):
-    context = {}
     areas = Area.objects.all()
     providers = Provider.objects.filter(is_visible=True)
-    context['areas'] = areas
-    context['providers'] = providers
-    return render(request, 'pos/new_po.html', context)
+    return render(request, 'pos/new_po.html', {'areas': areas, 'providers': providers})
 
 def post_po(request):
     pass
