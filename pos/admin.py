270a7d83f25f81b936dd7d6a7c243ada5e3615f8
Roddy Gonzalez <roddy.gonzalez.89@gmail.com>
2016-12-13 11:05:52 -0300

    Update model and admin app

--- pos/admin.py
+++ pos/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Area, YapoUser, Provider, PurchaseOrder
+from .models import Area, YapoUser, Provider, PurchaseOrder, PurchaseOrderDetail
+
+
+class DetailInline(admin.StackedInline):
+    model = PurchaseOrderDetail
+    extra = 3
+
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    inlines = [DetailInline]
+
+
 
 admin.site.register(Area)
 admin.site.register(YapoUser)
 admin.site.register(Provider)
-admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
--- pos/models.py
+++ pos/models.py
@@ -3,58 +3,58 @@ from django.contrib.auth.models import User
 
 
 class Area(models.Model):
-    short_name = models.CharField(max_length=16)
-    long_name = models.CharField(max_length=128)
-    manager_name = models.CharField(max_length=128)
+    short_name = models.CharField(max_length=16, null=True, blank=True)
+    long_name = models.CharField(max_length=128, null=True, blank=True)
+    manager_name = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.long_name
 
 
 class YapoUser(models.Model):
-    area = models.ForeignKey(Area)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
 
 
 class Provider(models.Model):
-    previous_provider = models.ForeignKey('self')
+    previous_provider = models.ForeignKey('self', null=True, blank=True)
     is_visible = models.BooleanField()
-    name = models.CharField(max_length=128)
-    business = models.CharField(max_length=128)
-    rut = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
-    contact_name = models.CharField(max_length=128)
-    contact_area = models.CharField(max_length=128)
-    contact_email = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, null=True, blank=True)
+    business = models.CharField(max_length=128, null=True, blank=True)
+    rut = models.CharField(max_length=128, null=True, blank=True)
+    address = models.CharField(max_length=128, null=True, blank=True)
+    phone = models.CharField(max_length=128, null=True, blank=True)
+    contact_name = models.CharField(max_length=128, null=True, blank=True)
+    contact_area = models.CharField(max_length=128, null=True, blank=True)
+    contact_email = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class PurchaseOrder(models.Model):
-    yapo_user = models.ForeignKey(YapoUser)
-    provider = models.ForeignKey(Provider)
-    folio_number = models.CharField(max_length=128)
-    payment_conditions = models.CharField(max_length=128)
-    payment_method = models.CharField(max_length=128)
-    contract_number = models.CharField(max_length=128)
-    quotation_order = models.CharField(max_length=128)
-    total_price = models.IntegerField()
-    currency = models.CharField(max_length=128)
+    yapo_user = models.ForeignKey(YapoUser, null=True, blank=True)
+    provider = models.ForeignKey(Provider, null=True, blank=True)
+    folio_number = models.CharField(max_length=128, null=True)
+    payment_conditions = models.CharField(max_length=128, null=True, blank=True)
+    payment_method = models.CharField(max_length=128, null=True, blank=True)
+    contract_number = models.CharField(max_length=128, null=True, blank=True)
+    quotation_order = models.CharField(max_length=128, null=True, blank=True)
+    total_price = models.IntegerField(null=True, blank=True)
+    currency = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.folio_number
 
 
 class PurchaseOrderDetail(models.Model):
-    purchase_order = models.ForeignKey(PurchaseOrder)
-    description = models.CharField(max_length=128)
-    quantity = models.IntegerField()
-    price = models.IntegerField()
+    purchase_order = models.ForeignKey(PurchaseOrder, null=True, blank=True)
+    description = models.CharField(max_length=128, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
     currency = models.CharField(max_length=8)
 
     def __str__(self):
