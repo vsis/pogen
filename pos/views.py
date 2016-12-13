601be0da715f74123c766c93e5f6c57f346359e0
Roddy Gonzalez <roddy.gonzalez.89@gmail.com>
2016-12-13 17:22:15 -0300

    Added statics and improver "new_po" template

--- pos/views.py
+++ pos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import PurchaseOrder
+from .models import Area, Provider, PurchaseOrder
+
+class ListView(generic.ListView):
+    template_name = 'pos/pos_list.html'
+    context_object_name = 'pos_list'
+    
+    def get_queryset(self):
+        return PurchaseOrder.objects.all()
 
 class DetailView(generic.DetailView):
     model = PurchaseOrder
@@ -9,6 +16,10 @@ class DetailView(generic.DetailView):
 
 def new_po(request):
     context = {}
+    areas = Area.objects.all()
+    providers = Provider.objects.filter(is_visible=True)
+    context['areas'] = areas
+    context['providers'] = providers
     return render(request, 'pos/new_po.html', context)
 
 def post_po(request):
